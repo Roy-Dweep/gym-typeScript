3e597aa1a74cc497cd1f836deee0d40e57a67d31
Dweep roy <dweep.roy.2023@gmail.com>
2024-03-16 15:24:57 +0900

    first commit

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,107 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
+import { SelectedPage } from "@/shared/types";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRebbull from "@/assets/SponsorRedBull.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { motion } from "framer-motion";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+function Home({ setSelectedPage }: Props) {
+  const isAboveMediumscreens = useMediaQuery("(min-width: 1060px)");
+  return (
+    <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
+      {/* IMAGE AND MAIN HEADER */}
+      <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+        {/* MAIN HEADER */}
+        <div className="z-10 mt-32 md:basis3/5">
+          {/* HEADING */}
+          <motion.div
+            className="md:-mt-20"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="relative">
+              <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext ">
+                <img src={HomePageText} alt="home-page-text" />
+              </div>
+            </div>
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </motion.div>
+          {/* ACTION */}
+          <motion.div
+            className="mt-8 flex items-center gap-8 md:justify-start"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, x: -100 },
+              visible: { opacity: 1, x: 0 },
+            }}
+          >
+            <div className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white">
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+            </div>
+            <div className="hover:underline text-primary-500">
+              <AnchorLink
+                className="text-sm font-bold underline text-primary-500 hover:text-secondary-500"
+                onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                href={`#${SelectedPage.ContactUs}`}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
+            </div>
+          </motion.div>
+        </div>
+        {/* IMAGE */}
+        <motion.div
+          className="flex basis-3/5 justify-center md:z-10
+                    md:ml-40 md:mt-16 md:justify-items-end "
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: 100 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <img src={HomePageGraphic} alt="home-pageGraphic" />
+        </motion.div>
+      </div>
+      {/* SPONSORS */}
+      {isAboveMediumscreens && (
+        <div className="h-[150px] w-full bg-primary-100 py-10">
+          <div className="mx-auto w-5/6">
+            <div className="flex w-3/5 items-center justify-between gap-8">
+              <img src={SponsorRebbull} alt="SponsorRebbull" />
+              <img src={SponsorFortune} alt="SponsorFortune" />
+              <img src={SponsorForbes} alt="SponsorForbes" />
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
+}
+
+export default Home;
